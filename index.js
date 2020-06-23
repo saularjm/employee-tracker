fc21895319dcb18580992417f370e4e413c55d63
Saular Moaddeli <jake.moaddeli@gmail.com>
2020-06-23 10:40:45 -0700

    wrote skeleton for required functions, started writing queries

--- index.js
+++ index.js
@@ -25,50 +25,98 @@ connection.connect(function(err) {
 const init = function() {
     inquirer
     .prompt({
-      type: "list",
-      name: "startMenu",
-      message: "What would you like to do?",
-      choices: [
-        "View all employees",
-        "View all roles",
-        "View all departments",
-        "Add employee",
-        "Add department",
-        "Add role",
-        "Update employee role"
-      ]
+        type: "list",
+        name: "startMenu",
+        message: "What would you like to do?",
+        choices: [
+            "View all employees",
+            "View all roles",
+            "View all departments",
+            "Add employee",
+            "Add role",
+            "Add department",
+            "Update employee role",
+            "Exit"
+        ]
     })
     .then(function(ans) {
-      //console.log(answer);
+        //console.log(answer);
       
-      switch (ans.startMenu) {
-        case "View all employees":
-          viewEmps();
-          break;
+        switch (ans.startMenu) {
+            case "View all employees":
+                viewEmps();
+                break;
 
-        case "View all roles":
-          viewRoles();
-          break;
+            case "View all roles":
+                viewRoles();
+                break;
 
-        case "View all departments":
-          viewDepts();
-          break;
+            case "View all departments":
+                viewDepts();
+                break;
 
-        case "Add employee":
-          addEmp();
-          break;
+            case "Add employee":
+                addEmp();
+                break;
 
-        case "Add department":
-          addDept();
-          break;
+            case "Add role":
+                addRole();
+                break;
 
-        case "Add role":
-          addRole();
-          break;
+            case "Add department":
+                addDept();
+                break;
 
-        case "Update employee role":
-          updateRole();
-          break;
-      }
+            case "Update employee role":
+                updateRole();
+                break;
+
+            case "Exit":
+                connection.end();
+                break;
+        }
     });
 };
+
+const viewEmps = function() {
+    connection.query("SELECT * FROM employee INNER JOIN role ON employee.role_id = role.id INNER JOIN department ON role.department_id = department.id", function(err,res) {
+        if (err) throw err;
+
+        cTable(res);
+    })
+    init();
+}
+
+const viewRoles = function() {
+    connection.query("SELECT * FROM role", function(err,res) {
+        if (err) throw err;
+
+        cTable(res);
+    })
+    init();
+}
+
+const viewDepts = function() {
+    connection.query("SELECT * FROM department", function(err,res) {
+        if (err) throw err;
+
+        cTable(res);
+    })
+    init();
+}
+
+const addEmp = function() {
+    
+}
+
+const addRole = function() {
+    
+}
+
+const addDept = function() {
+    
+}
+
+const updateRole = function() {
+    
+}

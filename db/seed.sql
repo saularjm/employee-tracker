dc3256e6d0af7d423011a77ca3e2c87db4d8bae9
Saular Moaddeli <jake.moaddeli@gmail.com>
2020-06-23 10:47:14 -0700

    added seeds

---
+++ db/seed.sql
@@ -0,0 +1,20 @@
+USE employee_trackerDB;
+
+INSERT INTO department (name)
+VALUES 
+    ("Sales"),
+    ("Legal"),
+    ("Engineering");
+
+INSERT INTO role (title, salary, department_id)
+VALUES 
+    ("Manager", 200000, 1),
+    ("Lawyer", 150000, 2),
+    ("Developer", 120000, 3);
+
+
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES 
+    ("Moira", "Rose", 1, NULL),
+    ("Johnny", "Rose", 2, NULL),
+    ("David", "Rose", 3, NULL);

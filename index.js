11a3595a9e55a73554588a8b0bf25844ddc028e5
Saular Moaddeli <jake.moaddeli@gmail.com>
2020-06-24 17:27:38 -0700

    fixed view all sql query

--- index.js
+++ index.js
@@ -79,7 +79,8 @@ const init = function() {
 };
 
 const viewEmps = function() {
-    connection.query("SELECT * FROM employee INNER JOIN role ON employee.role_id = role.id INNER JOIN department ON role.department_id = department.id", function(err,res) {
+
+    connection.query("SELECT e.id, e.first_name, e.last_name, role.title, department.name AS department, role.salary, concat(m.first_name, ' ' ,  m.last_name) AS manager FROM employee e LEFT JOIN employee m ON e.manager_id = m.id INNER JOIN role ON e.role_id = role.id INNER JOIN department ON role.department_id = department.id ORDER BY ID ASC", function(err,res) {
         if (err) throw err;
         
         console.log("\n");
